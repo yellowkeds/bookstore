9feca93d0528f9d58c980f48c06d617ffcf59f46
yellowkeds <dkxvhhcpa1986>
2015-05-25 18:16:36 +0300

    Work with admin panel (templates)

--- registration/admin.py
+++ registration/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from .models import Registration, Contact
 
-# Register your models here.
+
 class UserInfo(admin.StackedInline):
     model = Contact
     extra = 1
 
+
 class UserInfoPosition(admin.ModelAdmin):
+    search_fields = ['email']
     list_display = ['id','login', 'email']
 
     fieldsets = [
